3bec0d68905d3f32fd1dc79d1664aabd9af686f0
Your Name <siddhpura306@gmail.com>
2023-05-24 11:04:08 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+$('#menu-btn').click(function () {
+    $('#nav').toggleClass('menu-expand-collapse');
+    $('#header').toggleClass('hide-logo');
+    $('#menu-btn').toggleClass('menu-btn-height');
+})
